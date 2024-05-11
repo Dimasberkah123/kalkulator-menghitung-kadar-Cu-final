e676b2e584345601a050341483bac9da4cee36ef
Dimasberkah123 <168402448+Dimasberkah123@users.noreply.github.com>
2024-05-12 00:12:27 +0700

    Update kalkulatorcepat.py

--- kalkulatorcepat.py
+++ kalkulatorcepat.py
@@ -135,9 +135,14 @@ def show_pendahuluan():
     st.markdown("<h1 style='color:indigo'>'۫  ..˖💬໒꒰ྀ Selamat Datang di Kalkulator Cepat Kadar Cu❗❗6꒱ྀིঌ₊", unsafe_allow_html=True)
     st.markdown('<hr style="border: none; height: 5px; background: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);"/>',
                 unsafe_allow_html=True)
-    image_path = 'chalis.jpeg'  # Ganti dengan path yang sesuai ke file gambar Anda
-    st.image(image_path, caption='''Putri Chalis Lestari
-                                    (2320544''')
+    image_path = '''dimas.jpeg
+                    rayyan.jpeg
+                    chalis.jpeg
+                    ratuu.jpg'''  # Ganti dengan path yang sesuai ke file gambar Anda
+    st.image(image_path, caption='''Dimas Farrel Arunajati (2320511)
+                                    Fadhlurrahman Rayyandani Shafwan (2320522)
+                                    Putri Chalis Lestari (2320544)
+                                    Ratu Amalia Zahara (2320551)''')
     st.write(''' Kalkulator Cepat Kadar Cu ini disusun oleh :
 1. Andiani Putri Wijiyanti (2320507)
 2. Azizah lintang Maylya (2320511)
